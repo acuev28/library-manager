d743f19bf1c5f814e50bc9ed4c948384618dd6f8
acuev28 <alejandrocuevas188@gmail.com>
2025-05-04 19:52:11 -0500

    Complete checkOutStatus function with input validation

--- library.cpp
+++ library.cpp
@@ -30,3 +30,60 @@ int main(){
     cout << endl;
 
 }
+
+//Adds book information the user library vector
+void addToLibrary(vector<vector<string> > &library, string book, string ID){
+    if (book.find(",") != string::npos){
+        cout << "The book title cannot contain commas." << endl;
+        return;
+    }
+    
+    vector<string> bookDetails;
+    bookDetails.push_back(book);
+    bookDetails.push_back(ID);
+    bookDetails.push_back("0");
+    library.push_back(bookDetails);
+
+    cout << "The Following Entry Was Added\n";
+    cout << "-----------------------------\n";
+    cout << book << " --- " << ID << " --- " << "In Library\n";
+}
+
+// Prints out the library books check out status
+void checkOutStatus(const vector<vector<string> > &library){
+    int inLibrary = 0;
+    int checkedOut = 0;
+    int onLoan = 0;
+    int unknown = 0;
+    int other = 0;
+
+    for (size_t i = 0; i < library.size(); i++){
+        string status = library[i][2];
+
+        switch(stoi(status)){
+            case 0:
+                inLibrary += 1;
+                break;
+            case 1:
+                checkedOut += 1;
+                break;
+            case 2:
+                onLoan += 1;
+                break;
+            case 3:
+                unknown += 1;
+                break;
+            default:
+                other += 1;
+        }
+    }
+    cout << "Your Current Library's Stats" << endl;
+    cout << "----------------------------" << endl;
+    cout << "Total Books: " << library.size() << endl;
+    cout << "   In Library: " << inLibrary << endl;
+    cout << "   Checked Out: " << checkedOut << endl;
+    cout << "   On Loan: " << onLoan << endl;
+    cout << "   Unknown: " << unknown << endl;
+    cout << "   Other: " << other << endl;
+
+}
